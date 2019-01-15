b85c660cbeec344a32a7b4f353265b79a9e5358a
Jesse Kleve <jesse.kleve@gmail.com>
2019-01-14 23:26:28 -0700

    cleaned up env

--- bash/env.bash
+++ bash/env.bash
@@ -1,33 +1,22 @@
 
 # OS
-OS=$(uname -s)
+export OS=$(uname -s)
 
-PATH=$PATH:$HOME/.local/bin:$HOME/bin:./
-PYTHONPATH=$PYTHONPATH:~/Code/python_modules
+export PATH=$PATH:$HOME/.local/bin:$HOME/bin:./
+export PYTHONPATH=$PYTHONPATH:~/Code/python_modules
 
-# Android
-ANDROID_HOME=~/code/Android
-JAVA_HOME=/usr/java/jdk1.8.0_131
-export ANDROID_HOME
-export JAVA_HOME
-PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-
-export PATH
-export PYTHONPATH
 export TERM=xterm-256color
 
-# Fuck cli program
-alias fuck='eval $(thefuck $(fc -ln -1)); history -r'
-alias FUCK='fuck'
+export LD_LIBRARY_PATH=/usr/local/lib
+
+export TZ=America/Denver
 
 MY_BASH_BLUE="\033[0;34m" #Blue
 MY_BASH_NOCOLOR="\033[0m"
 
 # HISTTIMEFORMAT uses strftime: http://man7.org/linux/man-pages/man3/strftime.3.html
-HISTTIMEFORMAT=`echo -e ${MY_BASH_BLUE}[%m/%d %H:%M] $MY_BASH_NOCOLOR `
-HISTSIZE=20000
-HISTFILESIZE=20000
+export HISTTIMEFORMAT=`echo -e ${MY_BASH_BLUE}[%m/%d %H:%M] $MY_BASH_NOCOLOR `
+export HISTSIZE=20000
+export HISTFILESIZE=20000
 
-LD_LIBRARY_PATH=/usr/local/lib
-
-export TZ=America/Denver
+unset MY_BASH_BLUE MY_BASH_NOCOLOR
