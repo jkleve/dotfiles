56c1e9837135d8d3a085de378756ad802ae79942
Jesse Kleve <jesse.kleve@gmail.com>
2025-11-18 09:07:29 -0700

    Make blade_logs unbuffered

--- bash/belvedere.bash
+++ bash/belvedere.bash
@@ -1,6 +1,6 @@
 
 blade_logs() {
-    sudo journalctl -f -u "Blade@$1" | sed -E 's/^([A-Z][a-z]{2} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*"message":"([^"]+)".*/\1: \2/' | sed 's/\\n/\n/g'
+    sudo journalctl -f -u "Blade@$1" | sed --unbuffered -E 's/^([A-Z][a-z]{2} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*"message":"([^"]+)".*/\1: \2/' | sed --unbuffered 's/\\n/\n/g'
 }
 
 which_blades() {
